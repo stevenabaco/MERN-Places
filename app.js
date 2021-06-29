dd09574e30e03169a867f151d6f2b92b9c346258
Steven Abaco <stevenabaco@gmail.com>
2021-06-29 10:36:36 -0500

    Add logic for handling errors

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 // Instantiate express
 const app = express();
-// Configure middleware 
+// Configure middleware
 app.use('/api/places', placesRoutes); // => /api/places...
-app.use('/api/users', usersRoutes); // => /api/users...
+
+app.use((error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({message: error.message || 'An unknown error occured!' });
+});
 
 app.listen(5000);
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const DUMMY_USERS = [
-	{
-		id: 'u1',
-		name: 'Steven Abaco',
-		image: 'https://www.stevenabaco.dev/static/media/me.4043ab91.jpg',
-		places: 3,
-	}
-];
+// const DUMMY_USERS = [
+// 	{
+// 		id: 'u1',
+// 		name: 'Steven Abaco',
+// 		image: 'https://www.stevenabaco.dev/static/media/me.4043ab91.jpg',
+// 		places: 3,
+// 	}
+// ];
 
-router.get('/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const user = DUMMY_USERS.find(u => {
-    return u.id === userId;
-  })
-  res.json({user}) // 
-})
+
 
 module.exports = router;
