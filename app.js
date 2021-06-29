5b3fcb30eeff2843633095dd45ad030938493e80
Steven Abaco <stevenabaco@gmail.com>
2021-06-29 01:14:06 -0500

    Add Places and Users routes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// Import third party packages
 const express = require('express');
 const bodyParser = require('body-parser');
-
+// Import dependencies
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+// Instantiate express
 const app = express();
-
-
+// Configure middleware 
+app.use('/api/places', placesRoutes); // => /api/places...
+app.use('/api/users', usersRoutes); // => /api/users...
 
 app.listen(5000);
