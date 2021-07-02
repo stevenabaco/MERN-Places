6d86ca00d42a5c63e94a1a43e6303ef8591d5601
Steven Abaco <stevenabaco@gmail.com>
2021-07-02 11:30:06 -0500

    Add Controllers

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// const DUMMY_USERS = [
-// 	{
-// 		id: 'u1',
-// 		name: 'Steven Abaco',
-// 		image: 'https://www.stevenabaco.dev/static/media/me.4043ab91.jpg',
-// 		places: 3,
-// 	}
-// ];
+const DUMMY_USERS = [
+	{
+		id: 'u1',
+		name: 'Steven Abaco',
+		image: 'https://www.stevenabaco.dev/static/media/me.4043ab91.jpg',
+		places: 3,
+	}
+];
 
 
 
