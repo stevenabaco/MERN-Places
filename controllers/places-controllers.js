e933769e91a97a2ba379b7014f6dd045a8982606
Steven Abaco <stevenabaco@gmail.com>
2021-07-05 12:28:44 -0500

    Add logic to Delete Places & Remove it from User

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -6,7 +6,7 @@ const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
 const Place = require('../models/place');
 const User = require('../models/user');
-const Mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 let DUMMY_PLACES = [
 	{
@@ -178,10 +178,13 @@ const updatePlace = async (req, res, next) => {
 };
 
 const deletePlace = async (req, res, next) => {
+
 	const placeId = req.params.pid;
+	
 	let place;
+	
 	try {
-		place = await Place.findById(placeId);
+		place = await Place.findById(placeId).populate('creator');
 	} catch (err) {
 		const error = new HttpError(
 			'Something went wrong, could not delete place',
@@ -190,8 +193,18 @@ const deletePlace = async (req, res, next) => {
 		return next(error);
 	}
 
+	if (!place) {
+		const error = new HttpError('Could not find place for this id.', 404)
+		return next(error);
+	}
+
 	try {
-		await place.remove();
+		const session = await mongoose.startSession();
+		session.startTransaction();
+		await place.remove({ session: session });
+		place.creator.places.pull(place); // Remove place from the User
+		await place.creator.save({session: session}) // Save the session
+		await session.commitTransaction(); // Commit all changes in Transaction
 	} catch (err) {
 		const error = new HttpError(
 			'Something went wrong, could not delete place',
