4ed6a62239ff12d90a1630409dbdf141a74a8520
Steven Abaco <stevenabaco@gmail.com>
2021-07-05 22:17:31 -0500

    Adjust logic to POST places

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -97,7 +97,7 @@ const login = async (req, res, next) => {
 		);
 		return next(error);
 	}
-	res.json({ message: 'Logged in successfully!' });
+	res.json({ message: 'Logged in successfully!', user: existingUser.toObject({ getters: true}) });
 };
 
 exports.getUsers = getUsers;
