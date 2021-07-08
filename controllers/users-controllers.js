7489e87b90bfd589c96aa4ccaa0df53a72cfd4b7
Steven Abaco <stevenabaco@gmail.com>
2021-07-08 09:59:55 -0500

    Setup password hashing and comparing with Bcrypt

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const HttpError = require('../models/http-error');
 // const { v4: uuidv4 } = require('uuid'); // Removed after adding mongoose
 const User = require('../models/user');
@@ -20,7 +21,6 @@ const getUsers = async (req, res, next) => {
 const signup = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-
 		return next(
 			new HttpError(
 				'Invalid inputs, please check the information you entered.',
@@ -50,11 +50,19 @@ const signup = async (req, res, next) => {
 		return next(error);
 	}
 
+	let hashedPassword;
+	try {
+		hashedPassword = await bcrypt.hash(password, 12); // Returns a promise, first arguement is what needs to be hashed. Second Arguement is how many rounds
+	} catch (err) {
+		const error = new HttpError('Could not create user, please try again', 500);
+		return nextZ(error);
+	}
+
 	const createdUser = new User({
 		name, // name: name
 		email,
 		image: req.file.path,
-		password,
+		password: hashedPassword,
 		places: [],
 	});
 
@@ -95,6 +103,27 @@ const login = async (req, res, next) => {
 		);
 		return next(error);
 	}
+
+	let isValidPassword = false;
+	try {
+		isValidPassword = await bcrypt.compare(password, existingUser.password); // Returns a BOOLEAN. Compares the entered password to the existing user password.
+	} catch (err) {
+		const error = new HttpError(
+			'Could not log you in, Please check your credentials and try again.',
+			500
+		);
+		return next(error);
+	}
+
+	if (!isValidPassword) {
+		const error = new HttpError(
+			'Invalid credentials, could not log you in.',
+			401
+		);
+	}
+
+	
+
 	res.json({
 		message: 'Logged in successfully!',
 		user: existingUser.toObject({ getters: true }),
