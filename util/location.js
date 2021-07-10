f8f4ee6b99c3c794074f9c377f2edd9168a2354f
Steven Abaco <stevenabaco@gmail.com>
2021-07-09 22:29:48 -0500

    Adjust Google env API

--- util/location.js
+++ util/location.js
@@ -6,7 +6,7 @@ async function getCoordsForAddress(address) {
 	const response = await axios.get(
 		`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
 			address
-		)}&key=${API_KEY}`
+		)}&key=${process.env.API_KEY}`
   );
   const data = response.data;
 
