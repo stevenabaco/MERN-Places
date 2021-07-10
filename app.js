62d20d88f37904651addf5e92bc5c67a0e1691c7
Steven Abaco <stevenabaco@gmail.com>
2021-07-10 12:56:31 -0500

    update frontend BUild

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static(path.join('public')));
 
 // CORS HEADERS -- ONLY NEED IF BACKEND SERVED ON SEPERATE SERVER
-// app.use((req, res, next) => {
-// 	res.setHeader('Access-Control-Allow-Origin', '*');
-// 	res.setHeader(
-// 		'Access-Control-Allow-Headers',
-// 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-// 	);
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+	next();
+});
 
 app.use('/api/places', placesRoutes); // => /api/places...
 app.use('/api/users', usersRoutes); // => /api/users...
